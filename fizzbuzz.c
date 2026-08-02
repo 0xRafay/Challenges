85fbffe526b930afa68160b149e51288c92b4ac9
0xRafay <abdulrafay.src@gmail.com>
2026-08-02 07:43:54 -0400

    feat: add fizzbuzz.c

---
+++ fizzbuzz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  for (int i = 0; i <= 100; i++) {
+    if (i != 0) {
+
+      if (i % 3 == 0) {
+        printf("Fizz, ");
+      }
+      else if (i % 5 == 0) {
+        printf("Buzz, ");
+      } else {
+        printf("%d, ", i);
+      }
+    }
+  }
+}
