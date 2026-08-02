db29ef5b15ecb072a8b28db574241f5f373df20a
0xRafay <abdulrafay.src@gmail.com>
2026-08-02 07:44:55 -0400

    feat(fizzbuzz): add condition for both to be true at once

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,11 +2,12 @@
 int main() {
   for (int i = 0; i <= 100; i++) {
     if (i != 0) {
-
+      if (i % 3 == 0 && i % 5 == 0) {
+        printf("FizzBuzz, ");
+      }
       if (i % 3 == 0) {
         printf("Fizz, ");
-      }
-      else if (i % 5 == 0) {
+      } else if (i % 5 == 0) {
         printf("Buzz, ");
       } else {
         printf("%d, ", i);
