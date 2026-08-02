45b2f549531f0340220c206470b151e800f09a37
0xRafay <abdulrafay.src@gmail.com>
2026-08-02 08:51:44 -0400

    feat(natural-nums): add dynamic input

--- basic/natural-numbers.c
+++ basic/natural-numbers.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main() {
   int input = 10;
+
+  printf("Sum Until: ");
+  scanf("%d", &input);
+
   int natural = 0;
 
-  for (int i = 0; i <= 10; i++) {
+  for (int i = 0; i <= input; i++) {
     natural += i;
   }
   printf("Sum of Natural Numbers: %d\n", natural);
