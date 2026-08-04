32caeaae67d619dc656de0c0291b8010e7fc0e23
0xRafay <abdulrafay.src@gmail.com>
2026-08-04 09:11:47 -0400

    feat(linked-list): add preappend func

--- intermediate/linked-list.c
+++ intermediate/linked-list.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node{
+typedef struct Node {
     int data;
-    struct Node* next;
-    struct Node* prev;
+    struct Node *next;
+    struct Node *prev;
 } Node;
 
-Node* creat_node(int data){
-    Node* allocatedNode = malloc(sizeof(Node));
+Node *create_node(int data) {
+    Node *allocatedNode = malloc(sizeof(Node));
 
     allocatedNode->data = data;
     allocatedNode->next = NULL;
@@ -16,10 +16,30 @@ Node* creat_node(int data){
 
     return allocatedNode;
 }
+// why Node** instead of Node*?
+// a double pointers lets us modify the original pointer
+// Node* head, would only be a copy of head,
+// Node** head, means the original head
+void preappend(int data, Node **head) {
+
+    Node *new_node = create_node(data);
+
+    // we are trying to move the initial head one ahead, and add the new node as
+    // the new head
+    new_node->next = *head;
+    *head = new_node;
+}
 
 int main() {
 
-    Node* head = creat_node(10);
+    // theoretically this is the starting of the linked list
+    Node *head = NULL;
+
+    preappend(590, &head);
+    preappend(20, &head);
+    preappend(40, &head);
+
+    Node *current = head;
 
     printf("Head: %d\n", head->data);
 
